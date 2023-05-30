a6fa8888c79a74fa7be521af4af9c671046e8717
shc812 <shc812@kaist.ac.kr>
2023-05-31 00:37:24 +0900

    feat: app

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Layout</h1>}>
+          <Route path="/" element={<h2>main</h2>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
