5181cae56de33d1752b094f5071835d2ed935ef5
shc812 <shc812@kaist.ac.kr>
2023-05-31 22:29:19 +0900

    feat: 기본구조 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { Main } from "./pages/Main";
+import { RebornFlame } from "./pages/RebornFlame";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={<h1>Layout</h1>}>
-          <Route path="/" element={<h2>main</h2>}>
-            test
-          </Route>
-        </Route>
+        <Route path="/" element={<Main />} />
+        <Route path="reborn_flame" element={<RebornFlame />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/RebornFlame.tsx
@@ -0,0 +1,14 @@
+/** @jsxImportSource @emotion/react */
+import styled from "@emotion/styled";
+
+export function RebornFlame(): JSX.Element {
+  return (
+    <RebornFlameContainer>
+      <h2>RebornFlame</h2>
+    </RebornFlameContainer>
+  );
+}
+
+const RebornFlameContainer = styled.div`
+  margin-left: 20px;
+`;
